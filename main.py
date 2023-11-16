1bfb042ccca6ac8951829fa3729257c700001477
Ryanmc99 <142263767+Ryanmc99@users.noreply.github.com>
2023-11-16 14:28:16 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,164 @@
+# content from kids can code: http://kidscancode.org/blog/
+
+# import libraries and modules
+import pygame as pg
+from pygame.sprite import Sprite
+import random
+from random import randint
+import os
+import math
+from settings import *
+from sprites import *
+
+
+vec = pg.math.Vector2
+
+# setup asset folders here - images sounds etc.
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, 'images')
+snd_folder = os.path.join(game_folder, 'sounds')
+
+class Game:
+    def __init__(self):
+        # init pygame and create a window
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("My Game...")
+        self.clock = pg.time.Clock()
+        self.running = True
+    
+    def new(self):
+        # create a group for all sprites
+        self.score = 0
+        self.all_sprites = pg.sprite.Group()
+        self.all_platforms = pg.sprite.Group()
+        self.all_mobs = pg.sprite.Group()
+        # instantiate classes
+        self.player = Player(self)
+        # add instances to groups
+        self.all_sprites.add(self.player)
+
+        for p in PLATFORM_LIST:
+            # instantiation of the Platform class
+            plat = Platform(*p)
+            self.all_sprites.add(plat)
+            self.all_platforms.add(plat)
+
+        for m in range(0,10):
+            m = Mob(randint(0, WIDTH), randint(0, math.floor(HEIGHT/2)), 20, 20, "normal")
+            self.all_sprites.add(m)
+            self.all_mobs.add(m)
+
+        self.run()
+    
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        self.cd.ticking()
+
+        # handling collision in main instead of the mob or player class in sprites file
+        mhits = pg.sprite.spritecollide(self.player, self.all_mobs, True)
+        if mhits:
+            print('this MOB collision happening in main')
+            self.player.health -= 1
+            # play coin when collide with mob - which is weird because I don't have coins...
+            self.coin_sound.play()
+
+        # if len(self.all_mobs) < 1:
+        #     print("we are out of mobs!")
+        self.all_sprites.update()
+        if self.player.pos.x < 0:
+            self.player.pos.x = WIDTH
+        if self.player.pos.x > WIDTH: 
+            self.player.pos.x = 0
+        
+        # move plats up in group as you reach a point on screen
+        # if self.player.pos.y < WIDTH/4:
+        #     for p in self.all_platforms:
+        #         p.rect.y += 25
+        #         self.ground.rect.y += 25
+
+        # this is what prevents the player from falling through the platform when falling down...
+        hits = pg.sprite.spritecollide(self.player, self.all_platforms, False)
+        if hits:
+            if hits[0].category == "moving":
+                self.player.vel.x = hits[0].vel.x
+            if self.player.vel.y > 0:
+                self.player.pos.y = hits[0].rect.top
+                self.player.vel.y = 0
+                print(self.player.vel.y)
+                print(self.player.acc.y)
+            elif self.player.vel.y < 0:
+                self.player.vel.y = -self.player.vel.y
+            
+        # checks to see if player collides specifically with the ground and sets him on top of it
+        ghits = pg.sprite.collide_rect(self.player, self.ground)
+        if ghits:
+            self.player.pos.y = self.ground.rect.top
+            self.player.vel.y = 0
+            if self.player.cd.delta == 2:
+                print(self.player.cd.delta)
+                self.player.cd.event_reset()
+                self.player.health -= 1
+
+        hits = pg.sprite.spritecollide(self.player2, self.all_platforms, False)
+        if hits:
+            if self.player2.vel.y > 0:
+                self.player2.pos.y = hits[0].rect.top
+                self.player2.vel.y = 0
+        # prevent player from jumping through bottom of plat in future update...
+        ghits = pg.sprite.collide_rect(self.player2, self.ground)
+        if ghits:
+            self.player2.pos.y = self.ground.rect.top
+            self.player2.vel.y = 0
+
+
+    def events(self):
+        for event in pg.event.get():
+        # check for closed window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+                
+    def draw(self):
+        ############ Draw ################
+        # draw the background screen
+        self.screen.fill(BLACK)
+        # draw all sprites
+        self.all_sprites.draw(self.screen)
+        self.draw_text("P1 - Health: " + str(self.player.health), 22, WHITE, WIDTH/2, HEIGHT/24)
+        # self.draw_text("P2 - Health: " + str(self.player2.health), 22, WHITE, WIDTH/2, HEIGHT/10)
+        self.draw_text("acc: " + str(round(self.player.acc.y, 2)), 22, WHITE, 100, HEIGHT/6)
+        self.draw_text("vel: " + str(round(self.player.vel.y, 2)), 22, WHITE, 100, HEIGHT/4)
+        self.draw_text("cooldown: " + str(self.cd.delta), 22, WHITE, 200, HEIGHT/4)
+        self.draw_text("Score: " + str(self.score), 22, WHITE, WIDTH/2, HEIGHT/10)
+        # buffer - after drawing everything, flip display
+        pg.display.flip()
+    
+    def draw_text(self, text, size, color, x, y):
+        font_name = pg.font.match_font('arial')
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x,y)
+        self.screen.blit(text_surface, text_rect)
+
+    def show_start_screen(self):
+        pass
+    def show_go_screen(self):
+        pass
+
+g = Game()
+while g.running:
+    g.new()
+
+
+pg.quit()
